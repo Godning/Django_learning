8a049a26ecaf8b93ae86a5e8725ccace5385b434
Godning <lizhaoning13@gmail.com>
2015-10-24 14:51:10 +0800

    add log module

--- Mywork/ssh/models.py
+++ Mywork/ssh/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class CMD(models.Model):
+    cmd = models.CharField(max_length=30)
+    result = models.CharField(max_length=1024)
+    
+    def __str__(self):
+        return self.cmd
--- Mywork/ssh/views.py
+++ Mywork/ssh/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.template.context_processors import request
 from django.http.response import HttpResponse
 import socket
+from ssh.models import CMD
 # Create your views here.
 s=None
 
@@ -16,19 +17,32 @@ def Connect(request):
     return HttpResponse("success")
 
 def SendCommand(request):
-    host = 'localhost'
-    port = 50007
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    cmd = request.POST.get('cmd')     
-    if cmd: 
-        if cmd == 'q':
-            return HttpResponse("exit")
-        s.sendall(cmd + '\n')   
-    else:
-        return HttpResponse("")
-    print 'send success'
-    data = s.recv(1024)
-    print u'Received', data
-    s.close()
-    return HttpResponse(data)
+    try:
+        host = 'localhost'
+        port = 50007
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        cmd = request.POST.get('cmd')     
+        if cmd: 
+            if cmd == 'q':
+                return HttpResponse("exit")
+            s.sendall(cmd + '\n')   
+        else:
+            return HttpResponse("")
+        print 'send success'
+        data = s.recv(1024)
+        print u'Received', data
+        cmd_new=CMD(cmd=cmd,result=data)
+        cmd_new.save()
+        s.close()
+        return HttpResponse(data)
+    except Exception,e:
+        return HttpResponse("Server is not connected!")
+
+def log(request):
+    try:
+        cmd_result=CMD.objects.all()
+        print cmd_result
+        return render_to_response('show.html',{'result':cmd_result})
+    except Exception,e:
+        return render_to_response('show.html',{'result':str(e)})
